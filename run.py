f9e7302d741730efcc44365668b91cee8cb7f470
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-19 15:45:48 +0100

    Add interval from config

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from time import sleep
 from janitor.tasks.labware_location.main import sync_changes_from_labwhere
 from janitor.config.logging import LOGGING
+from janitor.config.defaults import SYNC_JOB_INTERVAL_SEC
 
 logging.config.dictConfig(LOGGING)
 
 if __name__ == "__main__":
     sched = BackgroundScheduler(daemon=False)
-    sched.add_job(sync_changes_from_labwhere, "interval", seconds=300)
+    sched.add_job(sync_changes_from_labwhere, "interval", seconds=SYNC_JOB_INTERVAL_SEC)
     sched.start()
     while True:
         sleep(300)
