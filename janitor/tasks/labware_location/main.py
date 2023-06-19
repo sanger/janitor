65b9dbf26e5ba2677829f5de5cdad83d8727913d
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-19 12:39:38 +0100

    Add logging for invalid entry

--- janitor/tasks/labware_location/main.py
+++ janitor/tasks/labware_location/main.py
@@ -1,9 +1,11 @@
+import logging
 from os import path
 from janitor.db.database import Database, DbConnectionDetails
 from janitor.helpers.mysql_helpers import load_query
 from janitor.helpers.mlwh_helpers import sort_results
 
 wd = path.realpath(path.dirname(__file__))
+logger = logging.getLogger(__name__)
 
 get_locations_file = "get_labware_locations.sql"
 add_to_labware_locations_file = "write_to_labware_locations.sql"
@@ -34,6 +36,10 @@ def sync_changes_from_labwhere():
     )
 
     mlwh_entries, invalid_entries = sort_results(results)
+
+    if invalid_entries:
+        for entry in invalid_entries:
+            logger.warning(f"Found invalid entry: {entry}")
 
     db_mlwh.write_entries_to_table(
         load_query(path.join(wd, "sql_queries", add_to_labware_locations_file)),
