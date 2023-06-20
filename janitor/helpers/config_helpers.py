6b00546b24fb94d4b230927051be564fa09c715b
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-20 13:55:18 +0100

    Add config loading method

---
+++ janitor/helpers/config_helpers.py
@@ -0,0 +1,23 @@
+import os
+from importlib import import_module
+from types import ModuleType
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_config(settings_module: str = "") -> ModuleType:
+    """
+    Load configs from janitor/config folder using environment variable.
+
+    Arguments:
+        settings_module (str): the settings module to load
+
+    Returns:
+        [ModuleType]: config module, available to use via `config.<param>`
+    """
+    if not settings_module:
+        settings_module = os.environ["SETTINGS_MODULE"]
+
+    config = import_module(settings_module)
+    return config
