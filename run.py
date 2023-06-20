32bf4ad6d82ee3a4488fd22dfc4940dd4a8bb29a
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-20 14:01:11 +0100

    Add get_config method

--- run.py
+++ run.py
@@ -2,14 +2,16 @@ import logging.config
 from apscheduler.schedulers.background import BackgroundScheduler
 from time import sleep
 from janitor.tasks.labware_location.main import sync_changes_from_labwhere
-from janitor.config.logging import LOGGING
-from janitor.config.defaults import SYNC_JOB_INTERVAL_SEC
+from janitor.helpers.config_helpers import get_config
 
-logging.config.dictConfig(LOGGING)
+config = get_config()
+logging.config.dictConfig(config.LOGGING)
 
 if __name__ == "__main__":
     sched = BackgroundScheduler(daemon=False)
-    sched.add_job(sync_changes_from_labwhere, "interval", seconds=SYNC_JOB_INTERVAL_SEC)
+    sched.add_job(
+        sync_changes_from_labwhere, "interval", seconds=config.SYNC_JOB_INTERVAL_SEC
+    )
     sched.start()
     while True:
         sleep(300)
