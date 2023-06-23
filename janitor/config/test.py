22708ad9b086e48c7afd040b65b3567dfec4af50
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-23 14:28:04 +0100

    Add test configs

---
+++ janitor/config/test.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+from .defaults import *
+
+# setting here will overwrite those in 'defaults.py'
+SYNC_JOB_INTERVAL_SEC = 120
+SYNC_JOB_OVERLAP_SEC = 10
+
+LABWHERE_DB: DbConnectionDetails = {
+    "db_name": "janitor_tests_lw_input",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "username": "root",
+    "password": "",
+}
+
+MLWH_DB: DbConnectionDetails = {
+    "db_name": "janitor_tests_mlwh_output",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "username": "root",
+    "password": "",
+}
+
+# logging config
+LOGGING["loggers"]["janitor"]["level"] = "DEBUG"
+LOGGING["loggers"]["apscheduler"]["level"] = "DEBUG"
+LOGGING["handlers"]["console"]["level"] = "DEBUG"
