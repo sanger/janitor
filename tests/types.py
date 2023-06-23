93bc7c8b4136ff917c1664f5be95cd8bf018b045
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-23 01:20:27 +0100

    Add types for different tables

---
+++ tests/types.py
@@ -0,0 +1,51 @@
+from typing import TypedDict, Optional
+from datetime import datetime
+
+
+class LabwaresTableEntry(TypedDict):
+    id: int
+    barcode: str
+    location_id: Optional[int]
+    coordinate_id: Optional[int]
+
+
+class AuditsTableEntry(TypedDict):
+    id: int
+    auditable_id: int
+    auditable_type: str
+    user_id: int
+    updated_at: datetime
+
+
+class UsersTableEntry(TypedDict):
+    id: int
+    login: str
+
+
+class LocationsTableEntry(TypedDict):
+    id: int
+    barcode: str
+    parentage: str
+
+
+class CoordinatesTableEntry(TypedDict):
+    id: int
+    position: int
+    row: int
+    column: int
+    location_id: int
+
+
+class LabwareLocationTableEntry(TypedDict):
+    id: int
+    labware_barcode: str
+    location_barcode: str
+    full_location_address: str
+    coordinate_position: int
+    coordinate_row: int
+    coordinate_column: int
+    lims_id: str
+    stored_by: str
+    stored_at: datetime
+    created_at: datetime
+    updated_at: datetime
