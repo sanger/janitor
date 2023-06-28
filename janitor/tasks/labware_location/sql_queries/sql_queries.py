77522f4eb40e2d69199be7ceb4a07034bbf6566d
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-28 16:20:57 +0100

    Use pathlib to get paths to SQL queries

--- janitor/tasks/labware_location/sql_queries/sql_queries.py
+++ janitor/tasks/labware_location/sql_queries/sql_queries.py
@@ -1,12 +1,12 @@
-from os import path
+from pathlib import Path
 
 from janitor.helpers.mysql_helpers import load_query
 
-wd = path.realpath(path.dirname(__file__))
+SQL_FOLDER_PATH = Path("./janitor/tasks/labware_location/sql_queries")
 
 
 GET_LOCATIONS_FILE = "get_labware_locations.sql"
-GET_LOCATIONS_QUERY = load_query(path.join(wd, GET_LOCATIONS_FILE))
+GET_LOCATIONS_QUERY = load_query(SQL_FOLDER_PATH / GET_LOCATIONS_FILE)
 
 WRITE_TO_LABWARE_LOCATION_FILE = "write_to_labware_locations.sql"
-WRITE_TO_LABWARE_LOCATION_QUERY = load_query(path.join(wd, WRITE_TO_LABWARE_LOCATION_FILE))
+WRITE_TO_LABWARE_LOCATION_QUERY = load_query(SQL_FOLDER_PATH / WRITE_TO_LABWARE_LOCATION_FILE)
