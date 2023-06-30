a78f4f946d0209d455ca07ac3dc4111e2d75094f
Javid Ahmed <ja33@sanger.ac.uk>
2023-06-30 16:14:55 +0100

    Add imports and remove noqa F405

--- janitor/config/test.py
+++ janitor/config/test.py
@@ -1,10 +1,13 @@
+from janitor.types import DbConnectionDetails
+
 from .defaults import *  # noqa: F401, F403
+from .logging import LOGGING
 
 # setting here will overwrite those in 'defaults.py'
 SYNC_JOB_INTERVAL_SEC = 300
 SYNC_JOB_OVERLAP_SEC = 10
 
-LABWHERE_DB = DbConnectionDetails(  # noqa: F405
+LABWHERE_DB = DbConnectionDetails(
     db_name="janitor_tests_lw_input",
     host="127.0.0.1",
     port=3306,
@@ -12,7 +15,7 @@ LABWHERE_DB = DbConnectionDetails(  # noqa: F405
     password="",
 )
 
-MLWH_DB = DbConnectionDetails(  # noqa: F405
+MLWH_DB = DbConnectionDetails(
     db_name="janitor_tests_mlwh_output",
     host="127.0.0.1",
     port=3306,
@@ -21,6 +24,6 @@ MLWH_DB = DbConnectionDetails(  # noqa: F405
 )
 
 # logging config
-LOGGING["loggers"]["janitor"]["level"] = "DEBUG"  # noqa: F405
-LOGGING["loggers"]["apscheduler"]["level"] = "DEBUG"  # noqa: F405
-LOGGING["handlers"]["console"]["level"] = "DEBUG"  # noqa: F405
+LOGGING["loggers"]["janitor"]["level"] = "DEBUG"
+LOGGING["loggers"]["apscheduler"]["level"] = "DEBUG"
+LOGGING["handlers"]["console"]["level"] = "DEBUG"
