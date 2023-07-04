1ddde5e989f7662ec267790f597194989268404a
Javid Ahmed <ja33@sanger.ac.uk>
2023-07-04 16:30:43 +0100

    Update to use latest timestamp from labware_location

--- janitor/tasks/labware_location/main.py
+++ janitor/tasks/labware_location/main.py
@@ -1,9 +1,14 @@
 import logging
 import time
+from datetime import datetime
 
 from janitor.db.database import Database
 from janitor.helpers.mlwh_helpers import sort_results
-from janitor.tasks.labware_location.sql_queries.sql_queries import GET_LOCATIONS_QUERY, WRITE_TO_LABWARE_LOCATION_QUERY
+from janitor.tasks.labware_location.sql_queries.sql_queries import (
+    GET_LATEST_TIMESTAMP_QUERY,
+    GET_LOCATIONS_QUERY,
+    WRITE_TO_LABWARE_LOCATION_QUERY,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -13,10 +18,15 @@ def sync_changes_from_labwhere(config):
     logger.info("Starting sync labware locations task...")
     db_labwhere = Database(config.LABWHERE_DB)
     db_mlwh = Database(config.MLWH_DB)
-    sync_job_interval = config.SYNC_JOB_INTERVAL_SEC + config.SYNC_JOB_OVERLAP_SEC
+
+    latest_timestamp = db_mlwh.execute_query(GET_LATEST_TIMESTAMP_QUERY, {})[0][0]
+
+    if not latest_timestamp:
+        latest_timestamp = datetime.min
+
     results = db_labwhere.execute_query(
         GET_LOCATIONS_QUERY,
-        {"interval": str(sync_job_interval)},
+        {"latest_timestamp": latest_timestamp},
     )
 
     mlwh_entries, invalid_entries = sort_results(results)
