bc628e3d81cb735aea547005b84da29c26c5560c
Javid Ahmed <ja33@sanger.ac.uk>
2023-09-04 17:36:37 +0100

    Add docstrings

--- janitor/helpers/log_helpers.py
+++ janitor/helpers/log_helpers.py
@@ -7,11 +7,17 @@ TMP_FOLDER = Path("./janitor/tmp")
 
 
 def make_tmp_folder() -> None:
+    """
+    Make tmp folder if it doesn't exist.
+    """
     if not os.path.exists(TMP_FOLDER):
         os.mkdir(TMP_FOLDER)
 
 
 def save_job_timestamp(job_name: str, timestamp: datetime) -> None:
+    """
+    Save timestamp of last time job was run.
+    """
     make_tmp_folder()
     filepath = TMP_FOLDER / f"{job_name}_latest_timestamp.txt"
     with open(filepath, "w") as file:
@@ -19,9 +25,15 @@ def save_job_timestamp(job_name: str, timestamp: datetime) -> None:
 
 
 def load_job_timestamp(job_name: str) -> Optional[str]:
+    """
+    Load timestamp of last job run.
+
+    Returns:
+        {Optional[str]}: Timestamp of last job run
+    """
     try:
         filepath = TMP_FOLDER / f"{job_name}_latest_timestamp.txt"
         with open(filepath, "r") as file:
             return file.read()
-    except:
+    except Exception:
         return None
