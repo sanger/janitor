f0d99fc348f35a8e349d81b141ec7744894ae9c7
Javid Ahmed <ja33@sanger.ac.uk>
2023-09-13 21:19:46 +0100

    Create events database tables

--- tests/conftest.py
+++ tests/conftest.py
@@ -75,6 +75,7 @@ def mlwh_events_database(mlwh_events_creds):
 
     try:
         mysql_conn = Database(mlwh_events_creds)
+        clear_mlwh_events_tables(mysql_conn)
         yield mysql_conn
     finally:
         mysql_conn.close()
@@ -124,15 +125,12 @@ def mock_rabbit(config):
         yield mock_rabbit
 
 
-def drop_and_recreate_table(db, table_name, cols, extra_query=None):
+def drop_and_recreate_table(db, table_name, cols):
     drop_table_query = f"DROP TABLE IF EXISTS {table_name}"
     db.execute_query(drop_table_query, {})
 
     create_table_query = f"CREATE TABLE {table_name} ({cols});"
     db.execute_query(create_table_query, {})
-
-    if extra_query:
-        db.execute_query(extra_query, {})
 
 
 def clear_lw_tables(lw_database):
@@ -184,3 +182,106 @@ def clear_mlwh_tables(mlwh_database):
         updated_at datetime(6) NOT NULL
     """
     drop_and_recreate_table(db=mlwh_database, table_name=labware_location_table_name, cols=labware_location_cols)
+
+    # Iseq flowcell table
+    iseq_flowcell_table_name = "iseq_flowcell"
+    iseq_flowcell_cols = """
+        id_iseq_flowcell_tmp int(11) primary key NOT NULL AUTO_INCREMENT,
+        id_sample_tmp int(11) NOT NULL,
+        id_study_tmp int(11)
+    """
+    drop_and_recreate_table(db=mlwh_database, table_name=iseq_flowcell_table_name, cols=iseq_flowcell_cols)
+
+    # Iseq product metrics table
+    iseq_product_metrics_table_name = "iseq_product_metrics"
+    iseq_product_metrics_cols = """
+        id_iseq_product varchar(255) NOT NULL UNIQUE,
+        id_iseq_flowcell_tmp int(11),
+        id_run int(11)
+    """
+    drop_and_recreate_table(
+        db=mlwh_database, table_name=iseq_product_metrics_table_name, cols=iseq_product_metrics_cols
+    )
+
+    # Iseq run status table
+    iseq_run_status_table_name = "iseq_run_status"
+    iseq_run_status_cols = """
+        id_run int(11) NOT NULL,
+        date datetime NOT NULL,
+        id_run_status_dict int(11) NOT NULL
+    """
+    drop_and_recreate_table(db=mlwh_database, table_name=iseq_run_status_table_name, cols=iseq_run_status_cols)
+
+    # Sample table
+    sample_table_name = "sample"
+    sample_cols = """
+        id_sample_tmp int(11) primary key NOT NULL AUTO_INCREMENT,
+        uuid_sample_lims varchar(255) UNIQUE,
+        supplier_name varchar(255)
+    """
+    drop_and_recreate_table(db=mlwh_database, table_name=sample_table_name, cols=sample_cols)
+
+    # Seq products irods locations table
+    seq_product_irods_locations_table_name = "seq_product_irods_locations"
+    seq_product_irods_locations_cols = """
+        id_product varchar(255) NOT NULL,
+        irods_root_collection varchar(255) NOT NULL,
+        irods_data_relative_path varchar(255),
+        irods_secondary_data_relative_path varchar(255)
+    """
+    drop_and_recreate_table(
+        db=mlwh_database, table_name=seq_product_irods_locations_table_name, cols=seq_product_irods_locations_cols
+    )
+
+    # Study table
+    study_table_name = "study"
+    study_cols = """
+        id_study_tmp int(11) primary key NOT NULL AUTO_INCREMENT,
+        name varchar(255)
+    """
+    drop_and_recreate_table(db=mlwh_database, table_name=study_table_name, cols=study_cols)
+
+
+def clear_mlwh_events_tables(mlwh_events_database):
+    # Events table
+    events_table_name = "events"
+    events_cols = """
+        id int(11) primary key NOT NULL AUTO_INCREMENT,
+        event_type_id int(11) NOT NULL,
+        occured_at datetime NOT NULL
+    """
+    drop_and_recreate_table(db=mlwh_events_database, table_name=events_table_name, cols=events_cols)
+
+    # Event types table
+    event_types_table_name = "event_types"
+    event_types_cols = """
+        id int(11) primary key NOT NULL AUTO_INCREMENT,
+        `key` varchar(255) NOT NULL UNIQUE
+    """
+    drop_and_recreate_table(db=mlwh_events_database, table_name=event_types_table_name, cols=event_types_cols)
+
+    # Roles table
+    roles_table_name = "roles"
+    roles_cols = """
+        event_id int(11) NOT NULL,
+        role_type_id int(11) NOT NULL,
+        subject_id int(11) NOT NULL
+    """
+    drop_and_recreate_table(db=mlwh_events_database, table_name=roles_table_name, cols=roles_cols)
+
+    # Role types table
+    role_types_table_name = "role_types"
+    role_types_cols = """
+        id int(11) NOT NULL,
+        `key` varchar(255) NOT NULL UNIQUE
+    """
+    drop_and_recreate_table(db=mlwh_events_database, table_name=role_types_table_name, cols=role_types_cols)
+
+    # Subjects table
+    subjects_table_name = "subjects"
+    subjects_cols = """
+        id int(11) primary key NOT NULL AUTO_INCREMENT,
+        uuid varchar(255) NOT NULL UNIQUE,
+        friendly_name varchar(255) NOT NULL
+    """
+    drop_and_recreate_table(db=mlwh_events_database, table_name=subjects_table_name, cols=subjects_cols)
